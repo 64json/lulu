a2d796166b098069fa2c524aa31f087d6ceb0aff
Jinseo Park <jinseo@google.com>
2022-01-25 01:25:37 -0500

    Fix typo

--- src/data/ncr/index.js
+++ src/data/ncr/index.js
@@ -3,7 +3,7 @@ export const ncr = [{
   note: 'Thanks Lulu, Womas, and Aleks for the smooth onboarding experience.',
 }, {
   image: require('./IMG_2353.jpg'),
-  note: 'Googler wearing Gatherly in NCR 👀',
+  note: 'Googler wearing Gatherly at NCR 👀',
 }, {
   image: require('./IMG_2253.JPG'),
   note: 'Featured on their corporate TikTok ;)',
